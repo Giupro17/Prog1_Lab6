22ffa4f54b366cd741e0816155b8cfa5231c743a
Giuliano <gproietti07@icloud.com>
2025-03-07 09:40:51 -0500

    question 18,22

--- RandomTester.java
+++ RandomTester.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.ArrayList;
 
 /**
  * Write a description of class RandomTester here.
@@ -10,6 +11,7 @@ public class RandomTester
 {
     // instance variables - replace the example below with your own
     private Random rand_gen;
+    private ArrayList<String> responses;
 
     /**
      * Constructor for objects of class RandomTester
@@ -18,6 +20,19 @@ public class RandomTester
     {
         // initialise instance variables
         rand_gen = new Random();
+        responses = new ArrayList<>();
+        fillResponses();
+
+    }
+    
+    private void fillResponses(){ //22
+        responses.add("yes");
+        responses.add("no");
+        responses.add("maybe");
+        responses.add("Ill ask my mom");
+        responses.add("I don't know");
+        responses.add("We'll see");
+        
     }
 
     /**
@@ -31,4 +46,32 @@ public class RandomTester
         // put your code here
         System.out.println("New integer:" + rand_gen.nextInt(1000));
     }
-}
+    
+    public void printMultiRandom(int howMany){
+        int index = 0;
+        while (index < howMany){
+            printOneRandom();
+            index++;
+        }
+        
+    }
+    
+    public String getResponse(){  //18
+        int index = rand_gen.nextInt(3);
+        
+        if (index == 0 ){
+            return "yes";
+        } else if (index == 1) {
+            return "no";
+        } else{
+            return "maybe";
+        }
+    }
+    
+    public String getBetterResponse(int index){
+        index = rand_gen.nextInt(responses.size());
+        
+        return responses.get(index); 
+    }
+    }
+
